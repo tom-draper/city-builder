737d2fd472a27808ca72c3102285d1de07f07c26
Tom Draper <tomjdraper1@gmail.com>
2021-12-23 10:43:20 +0000

    moving water

--- app.js
+++ app.js
@@ -1,100 +1,65 @@
-// Stage(function(stage) {
-
-//   var Math = Stage.Math, Mouse = Stage.Mouse;
-
-//   stage.viewbox(300, 300);
-
-//   var j = 0, i = 0;
-//   var column = Stage.column().appendTo(stage);
-//   for (j = 0; j < 10; j++) {
-//     var row = Stage.row().appendTo(column).spacing(0);
-//     for (i = 0; i < 10; i++) {
-//       // colors as frames
-//       // var cell = Stage.create();
-//       // cell.pin({
-//       //   name: "name",
-//       //   color: 'red',
-//       //   width: 10,
-//       //   height: 10,
-//       // })
-//       let cell = Stage.anim('dark');
-//       cell.appendTo(row);
-//       // var cell = Stage.anim('light').appendTo(row);
-//       cell.on(Mouse.CLICK, function(point) {
-//         this.gotoFrame(1);
-//         // console.log(this)
-//       });
-//     }
-//   }
-
-// });
-var grid = createGrid();
-
-var currentObj = 'lake';
-var mouseDown = false;
 
 function turnDraggedCellToObj() {
   if (mouseDown) {
     let cell = document.elementFromPoint(window.event.clientX, window.event.clientY);
-    if (currentObj == 'road') {
-      turnCellToRoad(cell);
-    } else {
-      turnCellToCurrentObj(cell)
+    turnCellToObj(cell)
+  }
+}
+
+function objSize(obj) {
+  if (obj == 'road') {
+    return [2, 2]
+  } else {
+    return [1, 1]
+  }
+}
+
+function randWater() {
+  return 'water-' +  (Math.floor((Math.random() * 3)) + 1);
+}
+
+function turnCellToObj(cell) {
+  let obj = currentObj;
+
+  [w, h] = objSize(obj);
+
+  for (i = 0; i < h; i++) {
+    for (j = 0; j < w; j++) {
+      if (obj == 'water') {
+        obj = randWater()
+      }
+      grid[cell.y+i][cell.x+j].className = "cell " + obj
     }
   }
 }
 
-
-function clearChunkClasses(cell) {
-  cell.className = "cell";
-  grid[cell.y][cell.x+1].className = "cell";
-  grid[cell.y+1][cell.x].className = "cell";
-  grid[cell.y+1][cell.x+1].className = "cell";
-}
-function turnCellToRoad(cell) {
-  clearChunkClasses(cell);
-
-  cell.classList.add('road');
-  grid[cell.y][cell.x+1].classList.add('road');
-  grid[cell.y+1][cell.x].classList.add('road');
-  grid[cell.y+1][cell.x+1].classList.add('road');
-}
-
-function turnCellToCurrentObj(cell) {
-  cell.className = "cell";
-  cell.classList.add(currentObj);
-  // grid[cell.y][cell.x+1].classList.add(currentObj);
-  // grid[cell.y+1][cell.x].classList.add(currentObj);
-  // grid[cell.y+1][cell.x+1].classList.add(currentObj);
-}
-
-function turnCellToObj(cell) {
-  if (currentObj == 'road') {
-    turnCellToRoad(cell)
-  } else {
-    turnCellToCurrentObj(cell)
-  }
-}
-
 function createArray(length) {
-  var arr = new Array(length || 0),
+  let arr = new Array(length || 0),
       i = length;
 
   if (arguments.length > 1) {
-      var args = Array.prototype.slice.call(arguments, 1);
+      let args = Array.prototype.slice.call(arguments, 1);
       while(i--) arr[length-1 - i] = createArray.apply(this, args);
   }
 
   return arr;
 }
 
+function divSize(divID) {
+  var div = document.getElementById(divID);
+
+  var width = div.clientWidth || div.offsetWidth || (div.getBoundingClientRect()).width;
+  var height= div.clientHeight || div.offsetHeight  || (div.getBoundingClientRect()).height;
+  return [width, height]
+}
+
 function createGrid() {
-  let grid = createArray(120, 100);
+  let grid = createArray(w, h);
 
   let container = document.getElementById("main");
-  for (i = 0; i < 100; i++) {
-    for (j = 0; j < 120; j++) {
-      var cell = document.createElement("div");
+  for (i = 0; i < h; i++) {
+    for (j = 0; j < w; j++) {
+      let cell = document.createElement("div");
       cell.classList.add("cell", "neutral");
       cell.y = i;
       cell.x = j;
@@ -115,3 +80,30 @@ function createGrid() {
   return grid
 }
 
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function randomBlue() {
+  let r = randInt(28, 40);
+  let g = randInt(165, 185);
+  let b = randInt(240, 255);
+  return 'rgb(' + r + ',' + g+ ',' + b + ')'
+}
+
+function gameLoop() {
+  document.documentElement.style.setProperty('--water-1', randomBlue());
+  document.documentElement.style.setProperty('--water-2', randomBlue());
+  document.documentElement.style.setProperty('--water-3', randomBlue());
+
+  setTimeout(gameLoop, 2000);
+}
+
+var w = 220;
+var h = 115;
+var currentObj = 'water';
+var mouseDown = false;
+
+var grid = createGrid();
+
+setTimeout(gameLoop, 2000);
