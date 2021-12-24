b45208de6a43d127a9d6b7321f78a4f81aea2120
Tom Draper <tomjdraper1@gmail.com>
2021-12-24 11:20:55 +0000

    forest and snow

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-
 async function turnDraggedCellToObj() {
   if (mouseDown) {
-    await turnCellToObj(document.elementFromPoint(window.event.clientX, window.event.clientY));
+    await turnCellToObj(
+      document.elementFromPoint(window.event.clientX, window.event.clientY)
+    );
   }
 }
 
@@ -23,12 +24,12 @@ async function applyObj(x, y) {
   if (currentObj == "water") {
     objClass = "water-" + rand1toN(3);
     if (!waterPlaced) {
-      waterPlaced = true;
+      waterPlaced = true;  // Begin animation
     }
-  } else if (currentObj == "grass") {
-    objClass = "grass-" + rand1toN(5);
-  } else if (currentObj == "hedge") {
-    objClass = "hedge-" + rand1toN(3);
+  } else if (currentObj == "grass" || (currentObj == "forest")) {
+    objClass = currentObj + "-" + rand1toN(5);
+  } else if (currentObj == "hedge" || currentObj == "snow") {
+    objClass = currentObj + "-" + rand1toN(3);
   }
 
   grid[x][y].className = "cell " + objClass;
@@ -37,53 +38,55 @@ async function applyObj(x, y) {
 async function fill(cell) {
   let x = cell.x;
   let y = cell.y;
-  if (cell.className == 'cell neutral' && onGrid(x, y)) {
-    await applyObj(y, x)
-    if (y < h-1) {
-      await fill(grid[y+1][x])
+  if (cell.className == "cell neutral" && onGrid(x, y)) {
+    await applyObj(y, x);
+    if (y < h - 1) {
+      await fill(grid[y + 1][x]);
     }
-    if (x < w-1) {
-      await fill(grid[y][x+1])
+    if (x < w - 1) {
+      await fill(grid[y][x + 1]);
     }
     if (y > 0) {
-      await fill(grid[y-1][x])
+      await fill(grid[y - 1][x]);
     }
     if (cell.x > 0) {
-      await fill(grid[y][x-1])
+      await fill(grid[y][x - 1]);
     }
   }
 }
 
 async function turnCellToObj(cell) {
-  if (fillMode && (currentObj == 'grass' || currentObj == 'water')) {
+  if (fillMode && (currentObj == "grass" || currentObj == "water" || currentObj == "forest" || currentObj == "snow")) {
     fill(cell);
   } else {
     [objWidth, objHeight] = objSize(currentObj);
     for (let i = 0; i < objHeight; i++) {
       for (let j = 0; j < objWidth; j++) {
-        await applyObj(cell.y+j, cell.x+i)
+        await applyObj(cell.y + j, cell.x + i);
       }
     }
   }
-
 }
 
 function setCurrentObj(obj) {
-  document.getElementById('roadBtn').classList.remove('active');
-  document.getElementById('waterBtn').classList.remove('active');
-  document.getElementById('grassBtn').classList.remove('active');
-  document.getElementById('hedgeBtn').classList.remove('active');
-  document.getElementById('fenceBtn').classList.remove('active');
-  document.getElementById('houseBtn').classList.remove('active');
-  document.getElementById('supermarketBtn').classList.remove('active');
-  document.getElementById('farmBtn').classList.remove('active');
-  document.getElementById('neutralBtn').classList.remove('active');
-  document.getElementById(obj + 'Btn').classList.add('active');
+  document.getElementById("roadBtn").classList.remove("active");
+  document.getElementById("waterBtn").classList.remove("active");
+  document.getElementById("grassBtn").classList.remove("active");
+  document.getElementById("forestBtn").classList.remove("active");
+  document.getElementById("snowBtn").classList.remove("active");
+  document.getElementById("hedgeBtn").classList.remove("active");
+  document.getElementById("fenceBtn").classList.remove("active");
+  document.getElementById("houseBtn").classList.remove("active");
+  document.getElementById("supermarketBtn").classList.remove("active");
+  document.getElementById("farmBtn").classList.remove("active");
+  document.getElementById("neutralBtn").classList.remove("active");
+  document.getElementById(obj + "Btn").classList.add("active");
   currentObj = obj;
 }
+
 function toggleFill() {
   fillMode = !fillMode;
-  document.getElementById('fillBtn').classList.toggle('active');
+  document.getElementById("fillBtn").classList.toggle("active");
 }
 
 function createArray(length) {
@@ -155,8 +158,13 @@ function updateWater() {
 }
 
 function neighbourIs(x, y, obj) {
-  let classN = "cell "+ obj;
-  return (grid[x+1][y].className == classN || grid[x][y+1].className == classN || grid[x-1][y].className == classN || grid[x][y-1].className == classN)
+  let classN = "cell " + obj;
+  return (
+    grid[x+1][y].className == classN ||
+    grid[x][y+1].className == classN ||
+    grid[x-1][y].className == classN ||
+    grid[x][y-1].className == classN
+  );
 }
 
 function driveLocations() {
@@ -164,15 +172,16 @@ function driveLocations() {
   for (let i = 0; i < h; i++) {
     for (let j = 0; j < w; j++) {
       if (grid[i][j].className == "cell road") {
-        if (i == 0 || j == 0 || i == h-1 || j == w-1 || neighbourIs(i, j, 'farm')) {
+        if (i == 0 || j == 0 || i == h - 1 || j == w - 1 || neighbourIs(i, j, "farm")
+        ) {
           locations.push([i, j]);
-        } else if (neighbourIs(i, j, 'house')) {
+        } else if (neighbourIs(i, j, "house")) {
           // Give x2 greater weight to house locations
           locations.push([i, j], [i, j]);
-        } else if (neighbourIs(i, j, 'supermarket')) {
-          // Give x6 greater weight to supermarket locations
-          locations.push([i, j], [i, j], [i, j], [i, j], [i, j], [i, j]);
-        } 
+        } else if (neighbourIs(i, j, "supermarket")) {
+          // Give x4 greater weight to supermarket locations
+          locations.push([i, j], [i, j], [i, j], [i, j]);
+        }
       }
     }
   }
@@ -180,33 +189,36 @@ function driveLocations() {
   return locations;
 }
 
-function selectRandLocation(locations) {
-  let selectedLoc = [null, null]
+function selectRandomLocation(locations) {
+  let selectedLoc = [null, null];
   if (locations.length > 0) {
-    selectedLoc = locations[Math.floor(Math.random() * locations.length)]
+    selectedLoc = locations[Math.floor(Math.random() * locations.length)];
   }
-  return selectedLoc
+  return selectedLoc;
 }
 
 function distance(x1, y1, x2, y2) {
-  return Math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+  return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 }
 
 function filterByDistance(startx, starty, locations, maxd) {
-  let filteredLocs = []
-  locations.forEach(element => {
-    [x, y] = element
+  let filteredLocs = [];
+  locations.forEach((element) => {
+    [x, y] = element;
     if (distance(startx, starty, x, y) > maxd) {
-      filteredLocs.push(element)
+      filteredLocs.push(element);
     }
   });
   return filteredLocs;
 }
 
-
 function indexOfNode(arr, value) {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0] == value[0] && arr[i][1] == value[1] && arr[i][2] == value[2]) {
+    if (
+      arr[i][0] == value[0] &&
+      arr[i][1] == value[1] &&
+      arr[i][2] == value[2]
+    ) {
       return i;
     }
   }
@@ -222,32 +234,31 @@ function removeNode(arr, node) {
 }
 
 function onGrid(x, y) {
-  return x >= 0 && y >= 0 && x < w && y < h
+  return x >= 0 && y >= 0 && x < w && y < h;
 }
 
 function carDrive(current, prev, step, path, carClass) {
   if (prev != null) {
     // Remove car from prev
     let [px, py] = prev;
-    grid[px][py].classList.remove(carClass)
+    grid[px][py].classList.remove(carClass);
   }
 
   if (current != null) {
     let [cx, cy] = current;
-    grid[cx][cy].classList.add(carClass)
-  
+    grid[cx][cy].classList.add(carClass);
+
     let next = null;
-    if (step < path.length-1) {
-      next = [path[step+1].x, path[step+1].y]
+    if (step < path.length - 1) {
+      next = [path[step + 1].x, path[step + 1].y];
     }
-  
-    setTimeout(function() {
-      carDrive(next, current, step+1, path, carClass);
+
+    setTimeout(function () {
+      carDrive(next, current, step + 1, path, carClass);
     }, 300);
-  } 
-  else {
+  } else {
     let [px, py] = prev;
-    grid[px][py].classList.remove(carClass)
+    grid[px][py].classList.remove(carClass);
   }
 }
 
@@ -257,7 +268,7 @@ function roadNetwork() {
   for (let i = 0; i < h; i++) {
     for (let j = 0; j < w; j++) {
       if (grid[i][j].className == "cell road") {
-        network[i][j] = 1; 
+        network[i][j] = 1;
       } else {
         network[i][j] = 0;
       }
@@ -267,12 +278,14 @@ function roadNetwork() {
 }
 
 function tryCarDrive() {
-  let startLocations = driveLocations()
-  let [sx, sy] = selectRandLocation(startLocations)
+  let startLocations = driveLocations();
+  let [sx, sy] = selectRandomLocation(startLocations);
+
   if (sx != null && sy != null) {
-    let finishLocations = driveLocations()
-    finishLocations = filterByDistance(sx, sy, finishLocations, 10)
-    let [fx, fy] = selectRandLocation(finishLocations)
+    let finishLocations = driveLocations();
+    let [fx, fy] = selectRandomLocation(
+      filterByDistance(sx, sy, finishLocations, 10)
+    );
 
     if (fx != null && fy != null) {
       let graph = new Graph(roadNetwork());
@@ -280,16 +293,21 @@ function tryCarDrive() {
       let finish = graph.grid[fx][fy];
       let path = astar.search(graph, start, finish);
       if (path.length > 0) {
-        carDrive([sx, sy], null, 0, path, 'car-' + rand1toN(5));
+        // If found a path that the car can take
+        console.log('Car driving from (' + sx + ', ', + sy + ') to (' + fx + ', ', + fy + ')');
+        carDrive([sx, sy], null, 0, path, "car-" + rand1toN(6));
       }
     }
   }
 
   // The more possible starting locations, the more frequently cars spawn
-  let waitTime =  120_000 - (startLocations.length*1000)
+  let waitTime = 160000 - startLocations.length * 100;
+  if (waitTime < 1000) {
+    waitTime = 1000;
+  }
+  console.log(waitTime, 'ms until next tryCarDrive');
   setTimeout(tryCarDrive, waitTime);
 }
-
 
 var w = 180;
 var h = 110;
@@ -301,4 +319,4 @@ var waterPlaced = false;
 var grid = createGrid();
 
 setTimeout(updateWater, 2000);
-setTimeout(tryCarDrive, 60_000);
+setTimeout(tryCarDrive, 30000);
